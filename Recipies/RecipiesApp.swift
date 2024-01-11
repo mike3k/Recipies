ff4afcea70e0345470b386535e4c7ad0a5fc3207
Mike Cohen <m@mcohen.me>
2024-01-11 14:08:46 -0800

    Initial Commit

---
+++ Recipies/RecipiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipiesApp.swift
+//  Recipies
+//
+//  Created by Mike Cohen on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
